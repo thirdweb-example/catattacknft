48c7fe8ac7d12d008c54c490f31b7601092e07b5
ikethirdweb <jorge@thirdweb.com>
2023-04-03 13:39:41 -0400

    move to public

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     esmExternals: "loose",
-    headers() {
-      return [
-        {
-          source: "/.well-known/apple-app-site-association",
-          headers: [{ key: "content-type", value: "application/json" }]
-        }
-      ];
-    }
   },
 };
 
