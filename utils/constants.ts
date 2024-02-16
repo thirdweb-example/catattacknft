80ed9a2fb4feaf3de5df620194e06a7c874e832f
Joaquim Verges <joaquim.verges@gmail.com>
2024-02-16 15:38:00 -0800

    switch to base-sepolia

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,6 @@
-import { BaseGoerli } from "@thirdweb-dev/chains";
+import { BaseSepoliaTestnet } from "@thirdweb-dev/chains";
 
 export const CLIENT_ID = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "";
-export const CHAIN = BaseGoerli;
-export const CONTRACT_ADDR = "0x5370B740616502979Fb1122E5e3896D41f6DAA1B";
-export const FACTORY_ADDR = "0x7f2A5F9C7B90C6B4F46038445118a9EB288722dd";
+export const CHAIN = BaseSepoliaTestnet;
+export const CONTRACT_ADDR = "0x5cA3b8E5B82D826aF6E8e9BA9E4E8f95cbC177F4";
+export const FACTORY_ADDR = "0x7b5ba9D46b53aae55e2c2E9b38d9AfF9a0b158F8";
